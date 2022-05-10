cc2847608d4d79d22aec252d420b7afeb5111f84
AlexSteiger <alexander.steiger@uni-rostock.de>
2022-05-10 12:10:16 +0200

    loop works

--- RainForecastAdapter.py
+++ RainForecastAdapter.py
@@ -17,76 +17,86 @@ api_key = "65d4508050d5008b768b660a688651ad"
  
 # base_url variable to store url
 base_url = "https://api.openweathermap.org/data/2.5/onecall?"
- 
-# Give city name
-#Turkey Fields:
-lat = "40.137442"
-lon = "28.383499"
-#Kassow, Germany:
+
+# Variables
+
+postgreSQLTable = ["bakir_weather","kassow_weather"]
+lat = ["40.137442","53.869024"]
+lon = ["28.383499","12.079214"]
+
+for i in range(0, 2):
+  print(postgreSQLTable[i])
+#postgreSQLTable = "turkey_weather";
+
+# Locations:
+# Bakir,Turkey Fields:
+#lat = "40.137442"
+#lon = "28.383499"
+# Kassow,Germany Fields:
 #lat = "53.869024"
 #lon = "12.079214"
 
+  # complete_url variable to store complete url address
+  complete_url = base_url + "appid=" + api_key + "&lat=" + lat[i] + "&lon=" + lon[i] + "&exclude=minutely,hourly,alerts"
+  #print(complete_url)
 
-# complete_url variable to store complete url address
-complete_url = base_url + "appid=" + api_key + "&lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts"
-print(complete_url)
-    
-# get method of requests module return response object
-response = requests.get(complete_url)
+  # get method of requests module return response object
+  response = requests.get(complete_url)
+
+  # convert json format data into python format data:
+  x = response.json()
+
+  #print(type(x))
+  #print(json.dumps(x, sort_keys=True, indent=4))
+
+  # read the response (x) into a dataframe:
+  df = pd.DataFrame(x['daily'])
+
+  # normalize nested 'temp' data:
+  df_temp = pd.json_normalize(df['temp'])
+
+  # add rain column, if not exists
+  if 'rain' not in df.columns:
+    df["rain"] = 0
+
+  # subset of the dataframe:
+  df      = df[["dt","humidity","dew_point","wind_speed","clouds","rain"]]
+  df_temp = df_temp['day']
+
+  # concat the two dataframes horizontally:
+  df = pd.concat([df, df_temp], axis=1)
+
+  # rename time column:
+  df = df.rename(columns={"dt":"date","day":"temp"})
  
-# convert json format data into python format data:
-x = response.json()
+  # kelvin to celsius
+  df['temp'] = df['temp'] - 273.15
+  df['dew_point'] = df['dew_point'] - 273.15
 
-print(type(x))
-#print(json.dumps(x, sort_keys=True, indent=4))
+  # fill NaN with Null
+  df = df.fillna(0)
 
-# Since x is a dictionary (<class 'dict'>) pd.read_json can be skipped
-# https://stackoverflow.com/questions/44980845/importing-json-into-pandas
+  # convert from unix time to python datetime:
+  df['date'] = pd.to_datetime(df['date'],unit='s')
 
-# read the response (x) into a dataframe:
-df = pd.DataFrame(x['daily'])
-#print(df)
+  #print(df.dtypes)
+  print(df)
 
-# normalize nested 'temp' data:
-df_temp = pd.json_normalize(df['temp'])
-#print("df_temp: ")
-#print(df_temp)
+  alchemyEngine   = create_engine('postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/addferti_weather', pool_recycle=3600);
+        # create_engine(dialect+driver://username:password@host:port/database)
 
-# subset of the dataframe:
-df      = df[["dt","humidity","dew_point","wind_speed","clouds","rain"]]
-df_temp = df_temp['day']
+  postgreSQLConnection = alchemyEngine.connect();
 
-# concat the two dataframes horizontally:
-df = pd.concat([df, df_temp], axis=1)
+  try:
+    print("try...")
+    frame = df.to_sql(postgreSQLTable[i], postgreSQLConnection, index=False, if_exists='append')
+    postgreSQLConnection.execute(
+        "DELETE FROM {} t WHERE EXISTS (SELECT FROM {} WHERE date = t.date AND ctid < t.ctid);"
+        .format(postgreSQLTable[i],postgreSQLTable[i]))
 
-# rename time column:
-df = df.rename(columns={"dt":"date"})
-
-# convert from unix time to python datetime:
-df['date'] = pd.to_datetime(df['date'],unit='s')
-#print(df)
-
-# change the datetime format to YYYY-MM-DD:
-df['date'] = df['date'].dt.strftime("%Y-%m-%d")
-#print(df)
-
-postgreSQLTable = "turkey_weather";
-alchemyEngine   = create_engine('postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/addferti_weather', pool_recycle=3600);
-                # create_engine(dialect+driver://username:password@host:port/database)
-
-postgreSQLConnection = alchemyEngine.connect();
-
-try:
-  print("try...")
-  psql_df = pd.read_sql('select * from turkey_weather', con=postgreSQLConnection)
-  #print("from psql: ")
-  #print(psql_df)
-  df_unique = pd.concat([df,psql_df]).drop_duplicates(subset=['date'],keep=False)
-  print(len(df_unique), "new rows added to the database")
-  frame = df_unique.to_sql(postgreSQLTable, postgreSQLConnection, index=False, if_exists='append');
-except:
-  print("except...")
-  print("create table", postgreSQLTable)
-  frame = df.to_sql(postgreSQLTable, postgreSQLConnection, index=False, if_exists='fail');
-finally:
-  postgreSQLConnection.close();
+  except:
+    print("except...")
+    print("create table", postgreSQLTable[i])
+    frame = df.to_sql(postgreSQLTable[i], postgreSQLConnection, index=False, if_exists='fail');
+  finally:
+    postgreSQLConnection.close();
